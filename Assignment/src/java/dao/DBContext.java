fbc213f2ab7e91f6ecc8e503f6b87b731eb673c3
trandinhdan <127070628+trandinhdan@users.noreply.github.com>
2024-04-09 09:55:53 +0700

    tao moi du an

---
+++ Assignment/src/java/dao/DBContext.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=ASSIGNMENT;encrypt=true;trustServerCertificate=true";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        System.out.println(db.connection != null);
+    }
+}
